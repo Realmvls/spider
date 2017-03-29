e1f928c4ee23ecdc9872161a6f9faff6c9c1b0cc
jochen <942814208@qq.com>
2017-03-29 13:16:56 +0800

    完整版1

---
+++ spider3.py
@@ -0,0 +1,10 @@
+# -*- coding : UTF-8 -*-
+import re
+import requests
+
+url = "http://www.vhiphop.com/"
+html = requests.get(url).text
+
+names = re.findall(r'<li><a href="(.*?)</a></li>',html,re.S)
+for name in names:
+    print name
